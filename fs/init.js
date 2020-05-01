45189d8cec25db3aa91efbc3a787f0af58324d13
Andreas Taube <andreas.taube@collect.ai>
2020-05-01 12:12:54 +0000

    Refactor step pwm duty resolving

--- fs/init.js
+++ fs/init.js
@@ -8,32 +8,52 @@ load('api_timer.js');
 let f = ffi('void dac_set(int)');
 let f2 = ffi('void dac_disable()');
 
-let ventl = {
-  step1: 0.37,  // 3.7V => 1
-  step2: 0.5,   // 5.0V => 2
-  step3: 0.57,  // 5.6V
-  step4: 0.66,  // 6.5V
-  step5: 0.72,  // 7.0V => 3
-  step6: 0.81,  // 7.9V
-  step7: 0.88   // 8.6V => 4
-};
-
 let pwmConfig = {
   pin: Cfg.get('app.pin'),
   frq: Cfg.get('app.frq'),
-  duty: Cfg.get('app.duty')
 };
+
+let ventlSteps = [
+  { step: 1, duty: 0.37 },  // 3.7V => 1
+  { step: 2, duty: 0.5 },   // 5.0V => 2
+  { step: 3, duty: 0.57 },  // 5.6V
+  { step: 4, duty: 0.66 },  // 6.5V
+  { step: 5, duty: 0.72 },  // 7.0V => 3
+  { step: 6, duty: 0.81 },  // 7.9V
+  { step: 7, duty: 0.88 }   // 8.6V => 4
+]
+
+function getDuty(step) {
+  const s = ventlSteps.find(function(s) {
+    return s.step === step;
+  })
+  if (s) {
+    return s.duty;
+  }
+  return ventlSteps[0].duty;
+}
+
+let state = {
+  step: 2,
+  bypass: 'off'
+}
 
 print('PWM Config', JSON.stringify(pwmConfig));
 
-PWM.set(pwmConfig.pin, pwmConfig.frq, pwmConfig.duty);
+PWM.set(pwmConfig.pin, pwmConfig.frq, getDuty(state.step));
 
 RPC.addHandler('Ventl.Set', function(args) {
   let step = args.step;
-  if (step < 0 || step > 1) {
-    return { error: 1000, message: 'Bad request' };
+
+  if (step < 1 || step > 7) {
+    return { error: 1000, message: 'Invalid step number, expected value between 1-7' };
   }
-  PWM.set(pwmConfig.pin, pwmConfig.frq, step);
+  state.step = step;
+  PWM.set(pwmConfig.pin, pwmConfig.frq, getDuty(step));
+});
+
+RPC.addHandler('Ventl.GetState', function(args) {
+  return { result: state }
 });
 
 RPC.addHandler('Ventl.Off', function(args) {
