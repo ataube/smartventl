8ee80fb26f4adeb072dce6ea46317a6706c2b9bf
Andreas Taube <andreas.taube@collect.ai>
2020-05-17 11:06:07 +0000

    Extend ventilator interface with off and max steps

--- fs/init.js
+++ fs/init.js
@@ -11,13 +11,15 @@ let pwmConfig = {
 };
 
 let ventlSteps = [
+  { step: 0, duty: 0 },     // 0V => off
   { step: 1, duty: 0.37 },  // 3.7V => 1
   { step: 2, duty: 0.5 },   // 5.0V => 2
   { step: 3, duty: 0.57 },  // 5.6V
   { step: 4, duty: 0.66 },  // 6.5V
   { step: 5, duty: 0.72 },  // 7.0V => 3
   { step: 6, duty: 0.81 },  // 7.9V
-  { step: 7, duty: 0.88 }   // 8.6V => 4
+  { step: 7, duty: 0.88 },  // 8.6V => 4
+  { step: 8, duty: 1 }      // 10V
 ];
 
 let getDuty = function(step) {
@@ -26,12 +28,13 @@ let getDuty = function(step) {
       return ventlSteps[i].duty;
     }
   }
-  return ventlSteps[0].duty;
+  return ventlSteps[2].duty;
 };
 
 let state = {
+  power: 'on',
   step: 2,
-  bypass: 'off'
+  bypass: 'off',
 };
 
 print('PWM Config', JSON.stringify(pwmConfig));
@@ -42,17 +45,26 @@ PWM.set(pwmConfig.pin, pwmConfig.frq, getDuty(state.step));
 RPC.addHandler('Ventl.Set', function(args) {
   let step = args.step;
 
-  if (step < 1 || step > 7) {
-    return { error: 1000, message: 'Invalid step number, expected value between 1-7' };
+  if (step < 0 || step > 8) {
+    return { error: 1000, message: 'Invalid step number, expected value between 1-8' };
   }
   state.step = step;
   PWM.set(pwmConfig.pin, pwmConfig.frq, getDuty(step));
 });
 
 RPC.addHandler('Ventl.GetState', function(args) {
-  return { result: state }
+  return { result: state };
 });
 
 RPC.addHandler('Ventl.Off', function(args) {
-  PWM.set(pwmConfig.pin, 0, 0)
+  PWM.set(pwmConfig.pin, 0, 0);
+  state.power = 'off';
+  state.step = 0;
+});
+
+RPC.addHandler('Ventl.On', function(args) {
+  let step = 2;
+  PWM.set(pwmConfig.pin, pwmConfig.frq, getDuty(step));
+  state.power = 'on';
+  state.step = step;
 });
